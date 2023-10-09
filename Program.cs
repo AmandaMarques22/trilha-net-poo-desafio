8e4a67938b68cc922839d45eb89d69fae12d372f
Amanda Marques <amandasilvamarques@hotmail.com>
2023-10-09 19:15:54 -0300

    exclusão do comentário

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
 Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 2", imei: "111111111", memoria: 64);
 nokia.Ligar();
